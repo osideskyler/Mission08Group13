e2e788eec0e50595f50e5a72044674f8271a1875
itloaf <jcarterk12k@gmail.com>
2026-02-26 23:13:06 -0700

    Created Landing, Create, and a placeholder Quadrants view. Create is functional, but does not hook up to the database yet.

---
+++ Mission08Group13/Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission08Group13.Models;
+using System.Collections.Generic;
+
+namespace Mission08Group13.Controllers
+{
+    public class TasksController : Controller
+    {
+      
+        public IActionResult Landing()
+        {
+            return View();
+        }
+
+        public IActionResult Quadrant()
+        {
+            return View();
+        }
+
+        private static List<TaskItem> _tasks = new List<TaskItem>();
+        private static int _nextId = 1;
+
+        public IActionResult Create(string submittedTask = null)
+        {
+            ViewBag.SubmittedTask = submittedTask;
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(TaskItem taskItem)
+        {
+            if (ModelState.IsValid)
+            {
+                taskItem.Id = _nextId++;
+                _tasks.Add(taskItem);
+
+                return RedirectToAction("Create", new { submittedTask = taskItem.Name });
+            }
+
+            return View(taskItem);
+        }
+    }
+}
---
+++ Mission08Group13/Models/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08Group13.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime? DueDate { get; set; }
+        [Required]
+        [Range(1,4)]
+        public int Quadrant { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public bool Completed { get; set; } = false;
+    }
+}
